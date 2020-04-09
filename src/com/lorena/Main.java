2b58db14b0c14307b45626cc514d1e854779b04b
Rusu Lorena <rusulorena@yahoo.com>
2020-04-09 19:21:20 +0300

    Tema Java

---
+++ src/com/lorena/Main.java
@@ -0,0 +1,18 @@
+package com.lorena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello");
+        System.out.println("Lorena");
+        int numar1= 3;
+        float numar2=5;
+        System.out.println(numar1+numar2);
+        System.out.println(numar1/numar2);
+        System.out.println(-5.0+8.0*6.0);
+        System.out.println((55.0+9)%9);
+        System.out.println(20.0+(-3.0)*5.0/8.0);
+        System.out.println(5.0+15.0/3.0*2.0-8.0%3.0);
+    }
+}
