46186fe4ed4a96df22b91ee968c07742f6fd8686
Rusu Lorena <rusulorena@yahoo.com>
2020-04-12 16:00:57 +0300

    Am adaugat text explicativ la operatiile de afisare a rezultatelor.

--- src/com/lorena/Main.java
+++ src/com/lorena/Main.java
@@ -8,11 +8,11 @@ public class Main {
         System.out.println("Lorena");
         int numar1= 3;
         float numar2=5;
-        System.out.println(numar1+numar2);
-        System.out.println(numar1/numar2);
-        System.out.println(-5.0+8.0*6.0);
-        System.out.println((55.0+9)%9);
-        System.out.println(20.0+(-3.0)*5.0/8.0);
-        System.out.println(5.0+15.0/3.0*2.0-8.0%3.0);
+        System.out.println("Suma dintre 3 si 5 este: "+ (numar1+numar2));
+        System.out.println("Impartirea dintre 3 si 5 este: "+ (numar1/numar2));
+        System.out.println("Rezultatul operatiei -5.0+8.0*6.0 este: "+(-5.0+8.0*6.0));
+        System.out.println("Rezultatul operatiei (55.0+9)%9 este: "+((55.0+9)%9));
+        System.out.println("Rezultatul operatiei 20.0+(-3.0)*5.0/8.0 este: "+ (20.0+(-3.0)*5.0/8.0));
+        System.out.println("Rezultatul operatiei 5.0+15.0/3.0*2.0-8.0%3.0 este: " + (5.0+15.0/3.0*2.0-8.0%3.0));
     }
 }
